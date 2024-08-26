ce0e9d1b8636ed198a6bc3b2d70fca7fe964f8ab
duc-hoang-kim <duchk@remitano.com>
2024-08-26 16:34:58 +0700

    remove redundant layout

---
+++ keyboards/crkbd/keymaps/duc-hoang/rules.mk
@@ -0,0 +1,7 @@
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = yes
+MOUSEKEY_ENABLE    = yes
+NKRO_ENABLE        = yes
+OLED_ENABLE        = yes
+OLED_DRIVER        = SSD1306
+LTO_ENABLE         = yes
